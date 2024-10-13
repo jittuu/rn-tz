679a639c6038e9ab575287b8e89b52a181f59c14
Soe Moe <hi@soe.moe>
2024-10-13 21:14:11 +0630

    add date-fns/fp for functional composition style

--- app/(tabs)/datefns.tsx
+++ app/(tabs)/datefns.tsx
@@ -1,17 +1,20 @@
+import { TZDate } from "@date-fns/tz";
+import { endOfDay, format, startOfDay, subWeeks } from "date-fns/fp";
+import { flow } from "lodash-es";
 import { SafeAreaView, StyleSheet } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
-import { TZDate } from "@date-fns/tz";
-import { endOfDay, format } from "date-fns";
 
-const formatFullDate = (date: TZDate) => {
-  return format(date, "dd/MM/yyyy hh:mm:ss a xxx");
+const formatFullDate = (date: Date) => {
+  return format("dd/MM/yyyy HH:mm:ss xxx")(date);
 };
 
 export default function DatefnsScreen() {
   const now = new Date();
   const bkk = new TZDate(now, "Asia/Bangkok");
   const ygn = new TZDate(now, "Asia/Yangon");
+
+  const startOfLastWeek = flow(subWeeks(1), startOfDay)(ygn);
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,6 +24,9 @@ export default function DatefnsScreen() {
       )}`}</ThemedText>
       <ThemedText>{`Ygn (end of day): ${formatFullDate(
         endOfDay(ygn)
+      )}`}</ThemedText>
+      <ThemedText>{`Ygn (start of last week): ${formatFullDate(
+        startOfLastWeek
       )}`}</ThemedText>
     </SafeAreaView>
   );
